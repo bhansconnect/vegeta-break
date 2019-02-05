2dc26e2df946209743c980c527fc58ba1e54d58f
Brendan Hansknecht <Brendan.Hansknecht@gmail.com>
2019-02-05 17:38:53 -0500

    Add RPS Accuracy

--- README.md
+++ README.md
@@ -1,30 +1,35 @@
 # vegeta-break
 
 ## Basics
+
 Vegeta-break is a comandline tool for discovering the max requests per second a service can handle while staying below a specific latency. It also outputs detailed latency curve files that can be load into [HdrHistogram Plotter](hdrhistogram.github.io/HdrHistogram/plotFiles.html) in order to better understand how the application acts under stress. The tool does this by amping up the number of requests per second of the application while insuring it is under a max latency.
 
- ```
- Usage: vegeta-break [OPTIONS] url
-  -duration duration
-    	Duration for each latency test (default 1m0s)
-  -percentile float
-    	The percentile that latency is measured at (default 99)
-  -rps int
-    	Starting requests per second (default 20)
-  -scaleup-percent float
-    	Percent of duration to scale up rps before each latency test (default 0.1)
-  -scaleup-steps int
-    	number of steps to go from 0 to max rps (default 10)
-  -sla duration
-    	Max acceptable latency (default 500ms)
- ```
+```
+Usage: vegeta-break [OPTIONS] url
+ -duration duration
+       Duration for each latency test (default 1m0s)
+ -percentile float
+       The percentile that latency is measured at (default 99.9)
+ -rps int
+       Starting requests per second (default 20)
+ -rps-accuracy float
+       How close the output should be to the correct rps. 100 is exact rps. 95 would be within 5% (default 100)
+ -scaleup-percent float
+       Percent of duration to scale up rps before each latency test (default 10)
+ -scaleup-steps int
+       number of steps to go from 0 to max rps (default 10)
+ -sla duration
+       Max acceptable latency (default 500ms)
+```
 
- ## Inspiration and Background
- The work by Gil Tene is a large inspiration for vegeta-break. Gil Tene teaches just how bad most latency measuring tools are through his talk [How NOT to Measure Latency](https://www.youtube.com/watch?v=lJ8ydIuPFeU). Other resources that talk about this issue in shorter article form are [Everything You Know About Latency Is Wrong](https://bravenewgeek.com/everything-you-know-about-latency-is-wrong/) and [Your Load Generator is Probably Lying to You](http://highscalability.com/blog/2015/10/5/your-load-generator-is-probably-lying-to-you-take-the-red-pi.html). I highly suggest you watch the talk if not at least read the articles. They show just how wrongly we measure and interpret latency. I hope that this tool can help people better compare websites changes based on latency and requests per second.
+## Inspiration and Background
 
- The core to this project is a tool called vegeta. Vegeta is a load testing tool that gets around the latency calculations issues that other tools have by asyncronously requesting for pages. If vegeta does not have enoug workers, it will spin up a new worker to ensure that request are sent on time. Thus, for vegeta, 50 requests per second means a request will be sent out every 20ms no matter the current latency and state of other requests.
+The work by Gil Tene is a large inspiration for vegeta-break. Gil Tene teaches just how bad most latency measuring tools are through his talk [How NOT to Measure Latency](https://www.youtube.com/watch?v=lJ8ydIuPFeU). Other resources that talk about this issue in shorter article form are [Everything You Know About Latency Is Wrong](https://bravenewgeek.com/everything-you-know-about-latency-is-wrong/) and [Your Load Generator is Probably Lying to You](http://highscalability.com/blog/2015/10/5/your-load-generator-is-probably-lying-to-you-take-the-red-pi.html). I highly suggest you watch the talk if not at least read the articles. They show just how wrongly we measure and interpret latency. I hope that this tool can help people better compare websites changes based on latency and requests per second.
+
+The core to this project is a tool called vegeta. Vegeta is a load testing tool that gets around the latency calculations issues that other tools have by asyncronously requesting for pages. If vegeta does not have enoug workers, it will spin up a new worker to ensure that request are sent on time. Thus, for vegeta, 50 requests per second means a request will be sent out every 20ms no matter the current latency and state of other requests.
 
 Lastly, [HTTP Load Testing with Vegeta (and a dash of Python)](https://serialized.net/2017/06/load-testing-with-vegeta-and-python/) is the basis of how I decided to test webpages. The author of this post actually made a script called vegeta-break. I took that script and expanded upon it to build the tool found here.
 
 ## What values should you use for sla and latency percentile?
+
 As a general rule of thumb, take the latency that you want X% of users to see and the add one to two 9s onto the end. For example, if you want 99% of users to see a latency of less than 1 second, then set percentile to 99.9 or 99.99 and sla to 1s.
--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"github.com/codahale/hdrhistogram"
+	ct "github.com/daviddengcn/go-colortext"
 	vegeta "github.com/tsenart/vegeta/lib"
 	histwriter "github.com/tylertreat/hdrhistogram-writer"
 )
@@ -26,12 +27,19 @@ func testRate(rps int, sla time.Duration, duration time.Duration, percentile flo
 	target.Header.Add("Accept-Encoding", "gzip, deflate")
 	targeter := vegeta.NewStaticTargeter(target)
 	attacker := vegeta.NewAttacker()
+	metrics := vegeta.Metrics{}
 
 	hist := hdrhistogram.New(1, 3600000, 3)
 
+	//Limit scaleup steps to numder of seconds for scaling up
+	//Vegeta doesn't seem to like shorter attacks
+	if int(scaleupDuration.Seconds()) < scaleupSteps {
+		scaleupSteps = int(scaleupDuration.Seconds())
+
+	}
 	scaleupRate := float64(rps) / float64(scaleupSteps)
 	subDuration := scaleupDuration / time.Duration(scaleupSteps)
-	fmt.Printf("Starting %d req/sec Scaleup for %s...\n", rps, scaleupDuration)
+	fmt.Printf("%s Starting %d req/sec Scaleup for %s...\n", time.Now().Format("[2006-01-02T15:04:05]"), rps, scaleupDuration)
 	for i := 0; i < scaleupSteps; i++ {
 		r := int(math.Ceil(float64(i+1) * scaleupRate))
 		if r > rps {
@@ -41,16 +49,13 @@ func testRate(rps int, sla time.Duration, duration time.Duration, percentile flo
 		for range attacker.Attack(targeter, vrate, subDuration, "Scale Up") {
 		}
 	}
-	fmt.Printf("Starting %d req/sec Load Test for %s...\n", rps, duration)
+	fmt.Printf("%s Starting %d req/sec Load Test for %s...\n", time.Now().Format("[2006-01-02T15:04:05]"), rps, duration)
 	vrate := vegeta.Rate{Freq: rps, Per: time.Second}
-	var errors int64
 	for res := range attacker.Attack(targeter, vrate, duration, "Latency Test") {
-		if res.Code <= 400 {
-			hist.RecordValue(res.Latency.Nanoseconds() / 1e3)
-		} else {
-			errors++
-		}
+		metrics.Add(res)
+		hist.RecordValue(res.Latency.Nanoseconds() / 1e3)
 	}
+	metrics.Close()
 
 	file, err := os.OpenFile(fmt.Sprintf("lat_%d.txt", rps), os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644)
 	if err != nil {
@@ -59,25 +64,28 @@ func testRate(rps int, sla time.Duration, duration time.Duration, percentile flo
 	defer file.Close()
 	histwriter.WriteDistribution(hist, histwriter.Logarithmic, 1e-3, file)
 
-	//adjust percentile for errors. For example if 1% errors, then adjusted 99% is 100%
-	var adjustedPercentile float64
-	if errors > 0 {
-		fmt.Printf("💥  %f%% of requests were errors\n", float64(100*errors)/float64(hist.TotalCount()+errors))
-		adjustedPercentile = (float64(hist.TotalCount()+errors) * percentile) / float64(hist.TotalCount())
-		if adjustedPercentile > 100 {
-			fmt.Printf("💥  Failed at %d req/sec (too many errors)\n", rps)
-			return false
-		}
-	} else {
-		adjustedPercentile = percentile
-	}
-
-	latency := time.Duration(hist.ValueAtQuantile(adjustedPercentile)) * time.Microsecond
-	if latency > sla {
-		fmt.Printf("💥  Failed at %d req/sec (latency %s)\n", rps, latency)
+	latency := time.Duration(hist.ValueAtQuantile(percentile)) * time.Microsecond
+	if 100*metrics.Success < percentile {
+		ct.Foreground(ct.Red, false)
+		fmt.Printf("%s Failed at %d req/sec (errors: %f%%)\n", time.Now().Format("[2006-01-02T15:04:05]"), rps, 100*(1-metrics.Success))
+		ct.Foreground(ct.White, false)
 		return false
 	}
-	fmt.Printf("✨  Success at %d req/sec (latency %s)\n", rps, latency)
+	if metrics.Rate < float64(rps) && float64(rps)-metrics.Rate > 1 {
+		ct.Foreground(ct.Red, false)
+		fmt.Printf("%s Failed at %d req/sec (Only managed to get to %f req/sec)\n", time.Now().Format("[2006-01-02T15:04:05]"), rps, metrics.Rate)
+		ct.Foreground(ct.White, false)
+		return false
+	}
+	if latency > sla {
+		ct.Foreground(ct.Red, false)
+		fmt.Printf("%s Failed at %d req/sec (latency %s)\n", time.Now().Format("[2006-01-02T15:04:05]"), rps, latency)
+		ct.Foreground(ct.White, false)
+		return false
+	}
+	ct.Foreground(ct.Green, false)
+	fmt.Printf("%s Success at %d req/sec (latency %s)\n", time.Now().Format("[2006-01-02T15:04:05]"), rps, latency)
+	ct.Foreground(ct.White, false)
 	return true
 }
 
@@ -86,17 +94,23 @@ func main() {
 	var duration time.Duration
 	var percentile float64
 	var scaleupPercent float64
+	var rpsAccuracy float64
 	var scaleupSteps int
 	var rps int
 	var sla time.Duration
 	flag.IntVar(&rps, "rps", 20, "Starting requests per second")
 	flag.DurationVar(&sla, "sla", 500*time.Millisecond, "Max acceptable latency")
 	flag.DurationVar(&duration, "duration", time.Minute, "Duration for each latency test")
-	flag.Float64Var(&percentile, "percentile", 99, "The percentile that latency is measured at")
+	flag.Float64Var(&percentile, "percentile", 99.9, "The percentile that latency is measured at")
 	flag.Float64Var(&scaleupPercent, "scaleup-percent", 10, "Percent of duration to scale up rps before each latency test")
+	flag.Float64Var(&rpsAccuracy, "rps-accuracy", 100, "How close the output should be to the correct rps. 100 is exact rps. 95 would be within 5%")
 	flag.IntVar(&scaleupSteps, "scaleup-steps", 10, "number of steps to go from 0 to max rps")
 	flag.Parse()
-	if flag.NArg() == 0 || rps <= 0 || scaleupPercent < 0 || scaleupPercent > 100 || scaleupSteps <= 0 || percentile < 0 || percentile > 100 {
+	if flag.NArg() == 0 || rps <= 0 || scaleupPercent < 0 || scaleupPercent > 100 || scaleupSteps <= 0 {
+		flag.Usage()
+		os.Exit(1)
+	}
+	if percentile < 0 || percentile > 100 || rpsAccuracy > 100 || rpsAccuracy <= 0 {
 		flag.Usage()
 		os.Exit(1)
 	}
@@ -117,7 +131,8 @@ func main() {
 	}
 
 	// next, do a binary search between okRate and nokRate
-	for (nokRate - okRate) > 1 {
+	rpsAccuracy = rpsAccuracy / 100.0
+	for float64(okRate)/float64(nokRate-1) < rpsAccuracy {
 		rps = (nokRate + okRate) / 2
 		if testRate(rps, sla, duration, percentile, scaleupDuration, scaleupSteps, url) {
 			okRate = rps
@@ -125,6 +140,10 @@ func main() {
 			nokRate = rps
 		}
 	}
-	fmt.Printf("➡️  Maximum Working Rate: %d req/sec\n", okRate)
-	os.Rename(fmt.Sprintf("lat_%d.txt", rps), fmt.Sprintf("lat_%d_best.txt", rps))
+	if nokRate-1 == okRate {
+		fmt.Printf("Maximum Working Rate: %d req/sec\n", okRate)
+	} else {
+		fmt.Printf("Maximum Working Rate Within: %d-%d req/sec\n", okRate, (nokRate - 1))
+	}
+	os.Rename(fmt.Sprintf("lat_%d.txt", okRate), fmt.Sprintf("lat_%d_best.txt", okRate))
 }
